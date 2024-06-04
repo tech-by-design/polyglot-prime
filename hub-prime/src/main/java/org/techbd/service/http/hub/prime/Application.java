1c91a02a9c3fe8bdcfe82942806ecdb4ee29ef43
Shahid N. Shah <shahid@shah.org>
2024-06-04 08:45:41 -0400

    refactor: use cached SFTP content

--- hub-prime/src/main/java/org/techbd/service/http/hub/prime/Application.java
+++ hub-prime/src/main/java/org/techbd/service/http/hub/prime/Application.java
@@ -2,10 +2,14 @@ package org.techbd.service.http.hub.prime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = { "org.techbd" })
 @EnableJpaRepositories(basePackages = "org.techbd.udi")
+@EnableCaching
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
