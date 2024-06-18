470081b60f516f647e0c416e24f9403a15d9a8b2
razakpm <abdulrazak-pm@netspective.in>
2024-06-18 18:25:21 +0530

    feat: disable login requirements in local environment #15

---
+++ hub-prime/src/main/java/org/techbd/service/http/NoAuthSecurityConfig.java
@@ -0,0 +1,47 @@
+package org.techbd.service.http;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+@Configuration
+@EnableWebSecurity
+@ConfigurationProperties(prefix = "spring.security.oauth2.client.registration.github")
+@Profile("localopen")
+public class NoAuthSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .anyRequest().permitAll())
+                .csrf().disable(); // Disable CSRF for simplicity in sandbox
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        // primarily setup for Swagger UI and OpenAPI integration
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*"); // Customize as needed
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
+}
