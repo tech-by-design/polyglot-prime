720fb8a3441c6f1f9c3808440a12c56804faca22
Alan Francis <alan.francis@citrusinformatics.com>
2024-06-29 11:15:24 +0530

    feat: initial diagnostics for csv ingestion through sftp #1

--- hub-prime/src/main/java/org/techbd/service/http/hub/prime/ux/DiagnosticsController.java
+++ hub-prime/src/main/java/org/techbd/service/http/hub/prime/ux/DiagnosticsController.java
@@ -1,24 +1,35 @@
 package org.techbd.service.http.hub.prime.ux;
 
-import static org.techbd.udi.auto.jooq.ingress.Tables.HUB_OPERATION_SESSION;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.techbd.orchestrate.sftp.SftpManager;
 import org.techbd.service.http.SandboxHelpers;
+import org.techbd.service.http.aggrid.ColumnVO;
+import org.techbd.service.http.aggrid.ServerRowsRequest;
+import org.techbd.service.http.aggrid.ServerRowsResponse;
+import org.techbd.service.http.aggrid.SqlQueryBuilder;
 import org.techbd.service.http.hub.prime.route.RouteMapping;
 import org.techbd.udi.UdiPrimeJpaConfig;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Nonnull;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -38,22 +49,72 @@ public class DiagnosticsController {
         this.udiPrimeJpaConfig = udiPrimeJpaConfig;
     }
 
-    @Operation(summary = "Recent HTTP Request/Response Diagnostics")
-    @GetMapping("/diagnostics.json")
-    @ResponseBody
-    public Page<?> adminDiagnosticsJson(@RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
+    
+    public Map<String, List<String>> getPivotValues(List<ColumnVO> pivotCols) {
+        Map<String, List<String>> pivotValues = new HashMap<>();
+        for (ColumnVO column : pivotCols) {
+            List<String> values = getValuesForField(column.getField());
+            pivotValues.put(column.getField(), values);
+        }
+        return pivotValues;
+    }
 
-        // TODO: don't select from HUB_OPERATION_SESSION, use the proper VIEW
-        final var DSL = udiPrimeJpaConfig.dsl();
-        final var result = DSL.selectFrom(HUB_OPERATION_SESSION).offset(page).limit(size).fetch();
-        return new PageImpl<>(result.intoMaps(), PageRequest.of(page, size), DSL.fetchCount(HUB_OPERATION_SESSION));
+    public List<String> getValuesForField(String field) {
+        List<String> values = Arrays.asList(field);
+        return values;
     }
 
     @GetMapping("/diagnostics")
     @RouteMapping(label = "Diagnostics", siblingOrder = 20)
-    public String adminDiagnostics(final Model model, final HttpServletRequest request) {
-        model.addAttribute("udiPrimaryDataSrcHealth", udiPrimeJpaConfig.udiPrimaryDataSrcHealth());
-        return presentation.populateModel("page/diagnostics", model, request);
+    public String adminDiagnostics() {
+        return "redirect:/diagnostics/sftp";
     }
+
+    @GetMapping("/diagnostics/sftp")
+    @RouteMapping(label = "CSV via SFTP(DB)", title = "CSV Diagnostics via SFTPs", siblingOrder = 10)
+    public String https(final Model model, final HttpServletRequest request) {
+        return presentation.populateModel("page/diagnostics/sftp", model, request);
+    }
+
+    
+    @Operation(summary = "SFTP Request/Response Diagnostics for Populating Grid")
+    @PostMapping(value = "/support/diagnostics/sftp.json", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ServerRowsResponse sftpDiagnostics(final @RequestBody @Nonnull ServerRowsRequest payload) {
+        // TODO: figure out how to write dynamic queries in jOOQ
+        // final var DSL = udiPrimeJpaConfig.dsl();
+        // final var result =
+        // DSL.selectFrom(INTERACTION_HTTP).offset(payload.getStartRow())
+        // .limit(payload.getEndRow() - payload.getStartRow() + 1).fetch();
+        // return ServerRowsResponse.createResponse(payload, result.intoMaps(), null);
+
+        final Map<String, List<String>> pivotValues = getPivotValues(payload.getPivotCols());
+
+        final var DSL = udiPrimeJpaConfig.dsl();
+        final var result = DSL.fetch(new SqlQueryBuilder().createSql(payload, "techbd_udi_ingress.orch_session_diagnostics",
+                pivotValues));
+
+        // create response with our results
+        return ServerRowsResponse.createResponse(payload, result.intoMaps(), pivotValues);
+
+    }
+
+    // @Operation(summary = "Recent HTTP Request/Response Diagnostics")
+    // @GetMapping("/diagnostics.json")
+    // @ResponseBody
+    // public Page<?> adminDiagnosticsJson(@RequestParam(defaultValue = "0") int page,
+    //         @RequestParam(defaultValue = "10") int size) {
+
+    //     // TODO: don't select from HUB_OPERATION_SESSION, use the proper VIEW
+    //     final var DSL = udiPrimeJpaConfig.dsl();
+    //     final var result = DSL.selectFrom(HUB_INTERACTION).offset(page).limit(size).fetch();
+    //     return new PageImpl<>(result.intoMaps(), PageRequest.of(page, size), DSL.fetchCount(HUB_INTERACTION));
+    // }
+
+    // @GetMapping("/diagnostics")
+    // @RouteMapping(label = "Diagnostics", siblingOrder = 20)
+    // public String adminDiagnostics(final Model model, final HttpServletRequest request) {
+    //     model.addAttribute("udiPrimaryDataSrcHealth", udiPrimeJpaConfig.udiPrimaryDataSrcHealth());
+    //     return presentation.populateModel("page/diagnostics", model, request);
+    // }
 }
