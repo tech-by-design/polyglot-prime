8940367fd93ad19b4e5ffc80a58f58aae717f49a
Alan Francis <alan.francis@citrusinformatics.com>
2024-07-29 13:49:01 +0530

    feat: intial screening & patient views for fhir content pages

--- udi-prime/src/main/postgres/ingestion-center/migrations/migrations.ts
+++ udi-prime/src/main/postgres/ingestion-center/migrations/migrations.ts
@@ -1,9 +1,10 @@
 import * as ic1 from "./migrate-basic-infrastructure.ts";
 import * as ic2 from "./migrate-interaction-fhir-view.ts";
 import * as ic3 from "./migrate-diagnostics-fhir-view.ts";
+import * as ic4 from "./migrate-content-fhir-view.ts";
 
 // Create an array containing the modules
-const ic = [ic1, ic2, ic3];
+const ic = [ic1, ic2, ic3, ic4];
 
 // Export the array
 export { ic };
