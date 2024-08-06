9e4c395a4c84b07effa4edd8579e71a8f99bd51b
Abdul Razak PM <razak@citrusinformatics.com>
2024-08-06 12:57:08 +0530

    chore resolve issues reported by CheckStyle #242 resolved issues EmptyLineSeparator

--- hub-prime/src/main/java/org/techbd/service/http/hub/prime/AppConfig.java
+++ hub-prime/src/main/java/org/techbd/service/http/hub/prime/AppConfig.java
@@ -11,6 +11,7 @@ public class AppConfig {
 
     public class Servlet {
         public static final String FHIR_CONTENT_TYPE_HEADER_VALUE = "application/fhir+json";
+        
         public class HeaderName {
             public class Request {
                 public static final String FHIR_STRUCT_DEFN_PROFILE_URI = Configuration.Servlet.HeaderName.PREFIX
