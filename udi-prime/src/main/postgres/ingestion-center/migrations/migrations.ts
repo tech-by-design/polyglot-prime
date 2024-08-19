a367ad2d4048d4ad544b1ed0c3d4d84a5befc420
Alan Francis <alan.francis@citrusinformatics.com>
2024-08-19 14:24:41 +0530

    feat: cronjob updates #35

--- udi-prime/src/main/postgres/ingestion-center/migrations/migrations.ts
+++ udi-prime/src/main/postgres/ingestion-center/migrations/migrations.ts
@@ -2,11 +2,12 @@ import * as ic1 from "./migrate-basic-infrastructure.ts";
 import * as ic2 from "./migrate-interaction-fhir-view.ts";
 import * as ic3 from "./migrate-diagnostics-fhir-view.ts";
 import * as ic4 from "./migrate-content-fhir-view.ts";
-import * as ic5 from "./migrate-models-dv.ts";
-import * as ic6 from "./migrate-ddl-stored-routine-interaction.ts";
+import * as ic5 from "./migrate-pg-cron.ts";
+import * as ic6 from "./migrate-models-dv.ts";
+import * as ic7 from "./migrate-ddl-stored-routine-interaction.ts";
 
 // Create an array containing the modules
-const ic = [ic1, ic2, ic3, ic4];
+const ic = [ic1, ic2, ic3, ic4, ic5];
 
 // Export the array
 export { ic };
