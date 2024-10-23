829b4556a76c262601f463149d20f141c1d92515
vinodaravind01 <vinod-aravindakshan@netspective.in>
2024-10-23 17:20:14 +0530

    feat: update supportEmail content with option to link to Access Request #576

--- hub-prime/src/main/java/org/techbd/service/http/GitHubUserAuthorizationFilter.java
+++ hub-prime/src/main/java/org/techbd/service/http/GitHubUserAuthorizationFilter.java
@@ -21,71 +21,72 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class GitHubUserAuthorizationFilter extends OncePerRequestFilter {
-  private static final String AUTH_USER_SESSION_ATTR_NAME = "authenticatedUser";
-  private static final String supportEmail = "Tech by Design Support <help@techbd.org>";
+    private static final String AUTH_USER_SESSION_ATTR_NAME = "authenticatedUser";
+    private static final String supportEmail = "help@techbd.org";
+    private static final String supportEmailDisplayName = "Tech by Design Support <" + supportEmail + ">";
 
-  @JsonIgnoreProperties(ignoreUnknown = true)
-  public record AuthenticatedUser(OAuth2User principal, GitHubUsersService.AuthorizedUser ghUser) implements Serializable {
-  }
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record AuthenticatedUser(OAuth2User principal, GitHubUsersService.AuthorizedUser ghUser) implements Serializable {
+    }
 
-  public static final Optional<AuthenticatedUser> getAuthenticatedUser(
-      final @NonNull HttpServletRequest request) {
-    final var sessionUser = (AuthenticatedUser) request.getSession(true)
-        .getAttribute(AUTH_USER_SESSION_ATTR_NAME);
-    return Optional.ofNullable(sessionUser);
-  }
+    public static final Optional<AuthenticatedUser> getAuthenticatedUser(
+            final @NonNull HttpServletRequest request) {
+        final var sessionUser = (AuthenticatedUser) request.getSession(true)
+                .getAttribute(AUTH_USER_SESSION_ATTR_NAME);
+        return Optional.ofNullable(sessionUser);
+    }
 
-  protected static final void setAuthenticatedUser(final @NonNull HttpServletRequest request,
-      final @NonNull AuthenticatedUser authUser) {
-    request.getSession(true).setAttribute(AUTH_USER_SESSION_ATTR_NAME, authUser);
-  }
+    protected static final void setAuthenticatedUser(final @NonNull HttpServletRequest request,
+            final @NonNull AuthenticatedUser authUser) {
+        request.getSession(true).setAttribute(AUTH_USER_SESSION_ATTR_NAME, authUser);
+    }
 
-  private final GitHubUsersService gitHubUsers;
+    private final GitHubUsersService gitHubUsers;
 
-  public GitHubUserAuthorizationFilter(final GitHubUsersService gitHubUsers) {
-    this.gitHubUsers = gitHubUsers;
-  }
+    public GitHubUserAuthorizationFilter(final GitHubUsersService gitHubUsers) {
+        this.gitHubUsers = gitHubUsers;
+            }
 
-  @Override
-  protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response,
-      @NotNull FilterChain filterChain)
-      throws ServletException, IOException {
-    final var sessionUser = getAuthenticatedUser(request);
-    if (sessionUser.isEmpty()) {
-      final var authentication = SecurityContextHolder.getContext().getAuthentication();
-      if (authentication != null && authentication.isAuthenticated()
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final var sessionUser = getAuthenticatedUser(request);
+        if (sessionUser.isEmpty()) {
+            final var authentication = SecurityContextHolder.getContext().getAuthentication();
+if (authentication != null && authentication.isAuthenticated()
           && !"anonymousUser".equals(authentication.getPrincipal().toString())) {
         final var gitHubPrincipal = (DefaultOAuth2User) authentication.getPrincipal();
-        final var gitHubLoginId = Optional.ofNullable(gitHubPrincipal.getAttribute("login")).orElseThrow();
-        final var gitHubAuthnUser = gitHubUsers.isAuthorizedUser(gitHubLoginId.toString());
-        if (!gitHubAuthnUser.isPresent()) {
-          response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-          response.getWriter()
-              .write("GitHub ID " + gitHubLoginId
-                  + " does not have permission to access this resource. Please send an email to "
-                  + supportEmail + " from your manager to have this GitHub ID added to the list of authorized users.");
-          return;
+                final var gitHubLoginId = Optional.ofNullable(gitHubPrincipal.getAttribute("login")).orElseThrow();
+                final var gitHubAuthnUser = gitHubUsers.isAuthorizedUser(gitHubLoginId.toString());
+                if (!gitHubAuthnUser.isPresent()) {
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    response.setContentType("text/html");
+                    response.getWriter().write("GitHub ID " + gitHubLoginId
+                            + " does not have permission to access this resource. Please fill out the <a href=\"https://techbd.org/submit-form/access-request\" target=\"_blank\"> Access Request Form </a> to request access. Once submitted, Tech by Design Support will review your request. For further assistance, contact Tech by Design Support at <<a href=\"mailto:"
+                            + supportEmailDisplayName + "\">" + supportEmail + "</a>>.");
+                    return;
+                }
+                setAuthenticatedUser(request, new AuthenticatedUser(gitHubPrincipal, gitHubAuthnUser.orElseThrow()));
+            }
         }
-        setAuthenticatedUser(request, new AuthenticatedUser(gitHubPrincipal, gitHubAuthnUser.orElseThrow()));
-      }
-    }
 
-    if (request.getRequestURI().startsWith("/actuator")) {
-      Optional<AuthenticatedUser> userOptional = getAuthenticatedUser(request);
-      if (userOptional.isPresent()) {
-        if (!userOptional.get().ghUser.isUserHasActuatorAccess()) {
-          response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-          response.setContentType("text/plain"); // Ensure content type is set to text/plain
-          response.getWriter().write("GitHub ID " + userOptional.get().ghUser.gitHubId()
-              + " does not have roles to access actuator. Please send an email to "
-              + supportEmail
-              + " from your manager, to add roles to access actuator from this GitHub ID.");
-          response.flushBuffer();
-          return; // Stop further processing of the request
+        if (request.getRequestURI().startsWith("/actuator")) {
+            Optional<AuthenticatedUser> userOptional = getAuthenticatedUser(request);
+            if (userOptional.isPresent()) {
+                if (!userOptional.get().ghUser.isUserHasActuatorAccess()) {
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    response.setContentType("text/plain"); // Ensure content type is set to text/plain
+                    response.getWriter().write("GitHub ID " + userOptional.get().ghUser.gitHubId()
+                            + " does not have roles to access actuator. Please send an email to "
+                            + supportEmailDisplayName
+                            + " from your manager, to add roles to access actuator from this GitHub ID.");
+                    response.flushBuffer();
+                    return; // Stop further processing of the request
+                }
+            }
         }
-      }
-    }
 
-    filterChain.doFilter(request, response);
-  }
+        filterChain.doFilter(request, response);
+    }
 }
