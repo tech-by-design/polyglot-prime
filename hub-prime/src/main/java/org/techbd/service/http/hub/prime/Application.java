6e3e2b2f7f27bcc5a83636707fe23d13bef5bdfb
jewelbonnie <jewel.bonnie@citrusinformatics.com>
2024-11-21 20:30:31 +0530

    Add CSV validation service and unit test implementation

--- hub-prime/src/main/java/org/techbd/service/http/hub/prime/Application.java
+++ hub-prime/src/main/java/org/techbd/service/http/hub/prime/Application.java
@@ -1,12 +1,19 @@
 package org.techbd.service.http.hub.prime;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.techbd.javapythonjunit.CsvValidationService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication(scanBasePackages = { "org.techbd" })
 @EnableJpaRepositories(basePackages = "org.techbd.udi")
 @EnableCaching
@@ -14,8 +21,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        // Get service from Spring context
+        CsvValidationService service = context.getBean(CsvValidationService.class);
+
+        try {
+            Map<String, Object> result = service.validateCsvGroup();
+            log.info("Validation result: {}", result);
+        } catch (Exception e) {
+            log.error("Error validating CSV files: ", e);
+        }
+    }
 
 }
