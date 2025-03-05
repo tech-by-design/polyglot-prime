08bdf05951c21e812bdcc74effb468d017073e75
Shreeja jobin <shreeja.jobin@citrusinformatics.com>
2025-03-05 14:59:22 +0530

    fix: fix package path in junit #1176

--- hub-prime/src/test/java/org/techbd/orchestrate/fhir/IgPublicationIssuesTest.java
+++ hub-prime/src/test/java/org/techbd/orchestrate/fhir/IgPublicationIssuesTest.java
@@ -324,28 +324,39 @@ public class IgPublicationIssuesTest {
     }
 
     private Map<String, FhirV4Config> getIgPackages() {
-        final Map<String, FhirV4Config> igPackages = new HashMap<>();        
-        FhirV4Config fhirV4Config = new FhirV4Config();        
+        final Map<String, FhirV4Config> igPackages = new HashMap<>();
+        FhirV4Config fhirV4Config = new FhirV4Config();
+    
+        // Base packages for external dependencies
         Map<String, String> basePackages = new HashMap<>();
-        basePackages.put("usCore", "ig-packages/fhir-v4/us-core/stu-7.0.0");
+        basePackages.put("us-core", "ig-packages/fhir-v4/us-core/stu-7.0.0");
         basePackages.put("sdoh", "ig-packages/fhir-v4/sdoh-clinicalcare/stu-2.2.0");
-        basePackages.put("uvSdc", "ig-packages/fhir-v4/uv-sdc/stu-3.0.0");        
-        Map<String, Map<String, String>> shinnyPackages = new HashMap<>();        
+        basePackages.put("uv-sdc", "ig-packages/fhir-v4/uv-sdc/stu-3.0.0");
+    
+        // Shinny Packages
+        Map<String, Map<String, String>> shinnyPackages = new HashMap<>();
+    
+        // Shinny version 1.2.3
         Map<String, String> shinnyV123 = new HashMap<>();
         shinnyV123.put("profile-base-url", "http://shinny.org/us/ny/hrsn");
-        shinnyV123.put("package-path", "ig-packages/shin-ny-ig/v1.2.3");
-        shinnyV123.put("ig-version", "1.2.3");        
-        Map<String, String> shinnyV130 = new HashMap<>();
-        shinnyV130.put("profile-base-url", "http://test.shinny.org/us/ny/hrsn");
-        shinnyV130.put("package-path", "ig-packages/shin-ny-ig/v1.3.0");
-        shinnyV130.put("ig-version", "1.3.0");        
+        shinnyV123.put("package-path", "ig-packages/shinny-packages/shinny/v1.2.3");
+        shinnyV123.put("ig-version", "1.2.3");
         shinnyPackages.put("shinny-v1-2-3", shinnyV123);
-        shinnyPackages.put("shinny-v1-3-0", shinnyV130);        
+    
+        // Test Shinny version 1.3.0
+        Map<String, String> testShinnyV130 = new HashMap<>();
+        testShinnyV130.put("profile-base-url", "http://test.shinny.org/us/ny/hrsn");
+        testShinnyV130.put("package-path", "ig-packages/shinny-packages/test-shinny/v1.3.0");
+        testShinnyV130.put("ig-version", "1.3.0");
+        shinnyPackages.put("test-shinny-v1-3-0", testShinnyV130);
+    
         fhirV4Config.setBasePackages(basePackages);
-        fhirV4Config.setShinnyPackages(shinnyPackages);        
-        igPackages.put("fhir-v4", fhirV4Config);        
+        fhirV4Config.setShinnyPackages(shinnyPackages);
+        igPackages.put("fhir-v4", fhirV4Config);
+    
         return igPackages;
     }
+    
 
     private String getIgVersion() {
         final String igVersion = "1.3.0";
