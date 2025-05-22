db9b18c07b54f4905c82a0dfe83f5f6f643c713e
Anoop Varma <anoopvarma.2000.p@gmail.com>
2025-05-22 19:17:49 +0530

    feat: bring nexus-ingestion-api project from infrastructure-prime repo #1473

---
+++ nexus-ingestion-api/src/test/java/org/techbd/nexusingestionapi/NexusIngestionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.techbd.nexusingestionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusIngestionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
