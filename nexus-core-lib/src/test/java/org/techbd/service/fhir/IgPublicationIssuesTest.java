167b58ef40ada3468ded7a6098386690d408264e
vinodaravind01 <vinod-aravindakshan@netspective.in>
2025-08-04 09:42:54 +0530

    test: update tests and examples for SHIN-NY IG v1.6.0  for QA #1749
    - Updated BaseIgValidationTest.java:
      - Changed test-shinny IG metadata from 1.5.3 to 1.6.0
    - Added new test in IgPublicationIssuesTest.java:
      - Validates test-shinny-examples/Bundle-TaskExampleMultipleEncounters.json
    - Added/updated example files under test-shinny-examples for v1.6.0

--- nexus-core-lib/src/test/java/org/techbd/service/fhir/IgPublicationIssuesTest.java
+++ nexus-core-lib/src/test/java/org/techbd/service/fhir/IgPublicationIssuesTest.java
@@ -126,6 +126,12 @@ public class IgPublicationIssuesTest extends BaseIgValidationTest {
         }
 
         @Test
+        @DisplayName("Validate Test SHIN-NY IG Task MultipleE ncounters ExampleFile")
+        void testValidateTestShinnyIG_TaskMultipleEncountersExampleFile() throws IOException {
+                validateFile("test-shinny-examples/Bundle-TaskExampleMultipleEncounters.json");
+        }
+        
+        @Test
         @DisplayName("Validate Test SHIN-NY IG Task Output Procedure ExampleFile")
         void testValidateTestShinnyIG_TaskOutputProcedureExampleFile() throws IOException {
                 validateFile("test-shinny-examples/Bundle-TaskOutputProcedureExample.json");
