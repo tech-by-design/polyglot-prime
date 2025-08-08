11b613d97fd29e797d03c465d4ebed5d2632befe
Shreeja jobin <shreeja-jobin@netspective.in>
2025-08-08 13:26:37 +0530

    feat(pix): add ITI-44 support for Patient Registry Duplicates Resolved (PRPA_IN201304UV02)
    - Implemented handler for PIX duplicate resolved interaction
    - Added acknowledgment generation via common AckService

--- nexus-ingestion-api/src/main/java/org/techbd/ingest/controller/DataIngestionController.java
+++ nexus-ingestion-api/src/main/java/org/techbd/ingest/controller/DataIngestionController.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
