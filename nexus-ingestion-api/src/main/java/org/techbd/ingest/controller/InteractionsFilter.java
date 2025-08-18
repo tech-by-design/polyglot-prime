55993afce12aa752e94d9433e56803e9b8a863a8
Shreeja jobin <shreeja-jobin@netspective.in>
2025-08-18 09:25:30 +0530

    fix: correctly log header value in incoming request.

--- nexus-ingestion-api/src/main/java/org/techbd/ingest/controller/InteractionsFilter.java
+++ nexus-ingestion-api/src/main/java/org/techbd/ingest/controller/InteractionsFilter.java
@@ -31,7 +31,7 @@ public class InteractionsFilter extends OncePerRequestFilter {
             while (headerNames.hasMoreElements()) {
                 var headerName = headerNames.nextElement();
                 var headerValue = origRequest.getHeader(headerName);
-                LOG.info("{} - Header: {} = {}", FeatureEnum.DEBUG_LOG_REQUEST_HEADERS, interactionId, headerName, headerValue);
+                LOG.info("{} - Header: {} = {} for interaction id: {}", FeatureEnum.DEBUG_LOG_REQUEST_HEADERS, headerName, headerValue, interactionId);
             }
         }
         chain.doFilter(origRequest, origResponse);
