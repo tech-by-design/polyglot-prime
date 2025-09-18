cadd8cd808ac99eaf0a3d66cdc2d3ffab9ea8dce
Vinod <vinod-aravindakshan@netspective.in>
2025-09-18 10:13:59 +0530

    test: update SHIN-NY IG tests and examples to v1.6.2 #2002
    - Updated BaseIgValidationTest.java:
      - Changed test-shinny IG metadata to 1.6.2
    - Updated/added example files under test-shinny-examples for v1.6.2

--- nexus-core-lib/src/test/java/org/techbd/service/fhir/BaseIgValidationTest.java
+++ nexus-core-lib/src/test/java/org/techbd/service/fhir/BaseIgValidationTest.java
@@ -80,11 +80,11 @@ public abstract class BaseIgValidationTest {
         shinny.put("ig-version", "1.5.3");
         shinnyPackages.put("shinny", shinny);
 
-        // Test Shinny version 1.6.1
+        // Test Shinny version 1.6.2
         Map<String, String> testshinny = new HashMap<>();
         testshinny.put("profile-base-url", "http://test.shinny.org/us/ny/hrsn");
-        testshinny.put("package-path", "ig-packages/shin-ny-ig/test-shinny/v1.6.1");
-        testshinny.put("ig-version", "1.6.1");
+        testshinny.put("package-path", "ig-packages/shin-ny-ig/test-shinny/v1.6.2");
+        testshinny.put("ig-version", "1.6.2");
         shinnyPackages.put("test-shinny", testshinny);
 
         fhirV4Config.setBasePackages(basePackages);
