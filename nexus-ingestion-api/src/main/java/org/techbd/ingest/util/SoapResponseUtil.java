aa8a8d3d5773619590f255a9f4aadaf8b582a793
Shreeja-dev <shreeja-jobin@netspective.in>
2025-09-18 20:14:00 +0530

    fix: correct timestamp difference in data and metadata bucket

--- nexus-ingestion-api/src/main/java/org/techbd/ingest/util/SoapResponseUtil.java
+++ nexus-ingestion-api/src/main/java/org/techbd/ingest/util/SoapResponseUtil.java
@@ -4,7 +4,10 @@ import java.util.Iterator;
 import java.util.UUID;
 
 import javax.xml.namespace.QName;
-import javax.xml.transform.dom.DOMResult;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
 
 import org.springframework.stereotype.Component;
 import org.springframework.ws.context.MessageContext;
@@ -12,8 +15,8 @@ import org.springframework.ws.soap.SoapHeader;
 import org.springframework.ws.soap.SoapHeaderElement;
 import org.springframework.ws.soap.SoapMessage;
 import org.techbd.ingest.config.AppConfig;
-
-import jakarta.xml.soap.SOAPElement;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 @Component
 public class SoapResponseUtil {
@@ -26,58 +29,62 @@ public class SoapResponseUtil {
         log.info("SoapResponseUtil initialized with AppConfig");
     }
 
-    public SoapMessage buildSoapResponse(String interactionId, MessageContext messageContext) {
-        log.info("Building SOAP response for interactionId={}", interactionId);
+ public SoapMessage buildSoapResponse(String interactionId, MessageContext messageContext) {
+    log.info("Building SOAP response for interactionId={}", interactionId);
 
-        try {
-            SoapMessage soapResponse = (SoapMessage) messageContext.getResponse();
-            SoapMessage soapRequest = (SoapMessage) messageContext.getRequest();
-            SoapHeader header = soapResponse.getSoapHeader();
+    try {
+        SoapMessage soapResponse = (SoapMessage) messageContext.getResponse();
+        SoapMessage soapRequest = (SoapMessage) messageContext.getRequest();
+        SoapHeader header = soapResponse.getSoapHeader();
 
-            String messageId = "urn:uuid:" + UUID.randomUUID();
-            String relatesTo = extractRelatesTo(soapRequest);
+        String messageId = "urn:uuid:" + UUID.randomUUID();
+        String relatesTo = extractRelatesTo(soapRequest);
 
-            if (relatesTo == null) {
-                relatesTo = "urn:uuid:unknown-incoming-message-id";
-                log.warn("RelatesTo header not found in request. Using fallback: {}", relatesTo);
-            }
-
-            var wsa = appConfig.getSoap().getWsa();
-            var techbd = appConfig.getSoap().getTechbd();
-
-            // Standard WS-Addressing headers
-            header.addHeaderElement(new QName(wsa.getNamespace(), "Action", wsa.getPrefix()))
-                    .setText(wsa.getAction());
-            header.addHeaderElement(new QName(wsa.getNamespace(), "MessageID", wsa.getPrefix()))
-                    .setText(messageId);
-            header.addHeaderElement(new QName(wsa.getNamespace(), "RelatesTo", wsa.getPrefix()))
-                    .setText(relatesTo);
-            header.addHeaderElement(new QName(wsa.getNamespace(), "To", wsa.getPrefix()))
-                    .setText(wsa.getTo());
-
-            // Create <techbd:Interaction> with attributes
-            SoapHeaderElement interactionHeader = header
-                    .addHeaderElement(new QName(techbd.getNamespace(), "Interaction", techbd.getPrefix()));
-
-            // Cast Result to DOMResult and extract SOAPElement
-            DOMResult domResult = (DOMResult) interactionHeader.getResult();
-            SOAPElement interactionElement = (SOAPElement) domResult.getNode().getFirstChild();
-
-            // Add attributes
-            interactionElement.setAttribute("InteractionID", interactionId);
-            interactionElement.setAttribute("TechBDIngestionApiVersion", appConfig.getVersion());
-
-            // marshaller.marshal(payload, soapResponse.getPayloadResult());
-
-            log.info("SOAP response built successfully for interactionId={}, version={}",
-                    interactionId, appConfig.getVersion());
-            return soapResponse;
-
-        } catch (Exception e) {
-            log.error("Failed to build SOAP response for interactionId={}", interactionId, e);
-            throw new RuntimeException("Error creating SOAP response", e);
+        if (relatesTo == null) {
+            relatesTo = "urn:uuid:unknown-incoming-message-id";
+            log.warn("RelatesTo header not found in request. Using fallback: {}", relatesTo);
         }
+
+        var wsa = appConfig.getSoap().getWsa();
+        var techbd = appConfig.getSoap().getTechbd();
+
+        // Standard WS-Addressing headers
+        header.addHeaderElement(new QName(wsa.getNamespace(), "Action", wsa.getPrefix()))
+              .setText(wsa.getAction());
+        header.addHeaderElement(new QName(wsa.getNamespace(), "MessageID", wsa.getPrefix()))
+              .setText(messageId);
+        header.addHeaderElement(new QName(wsa.getNamespace(), "RelatesTo", wsa.getPrefix()))
+              .setText(relatesTo);
+        header.addHeaderElement(new QName(wsa.getNamespace(), "To", wsa.getPrefix()))
+              .setText(wsa.getTo());
+
+        // Build <techbd:Interaction> as DOM element
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        Document doc = dbf.newDocumentBuilder().newDocument();
+
+        Element interactionElement = doc.createElementNS(
+                techbd.getNamespace(), techbd.getPrefix() + ":Interaction");
+
+        interactionElement.setAttribute("InteractionID", interactionId);
+        interactionElement.setAttribute("TechBDIngestionApiVersion", appConfig.getVersion());
+
+        // Write it into the SOAP header
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.transform(new DOMSource(interactionElement), header.getResult());
+
+        log.info("SOAP response built successfully for interactionId={}, version={}",
+                interactionId, appConfig.getVersion());
+
+        return soapResponse;
+
+    } catch (Exception e) {
+        log.error("Failed to build SOAP response for interactionId={}, version={}",
+                interactionId, appConfig.getVersion(), e);
+        throw new RuntimeException("Error creating SOAP response", e);
     }
+}
+
 
     private String extractRelatesTo(SoapMessage soapRequest) {
         String wsaNs = appConfig.getSoap().getWsa().getNamespace();
