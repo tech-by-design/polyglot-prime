4e2c6dc0fb024ab5e38636fb685e766cc0263ff9
sabith-nadakkavil <sabithnadakkavil@gmail.com>
2025-09-30 18:08:35 +0530

    refactor: update SourceType from HL7 to HL7V2 in relevant services #1695

--- nexus-core-lib/src/main/java/org/techbd/config/SourceType.java
+++ nexus-core-lib/src/main/java/org/techbd/config/SourceType.java
@@ -1,14 +1,14 @@
 package org.techbd.config;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public enum SourceType {
     FHIR,
     CSV,
-    HL7,
+    HL7V2,
     CCDA;
 
     public static List<SourceType> getAllSourceTypes() {
