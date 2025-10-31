6f73ef107cb8ad26c9d72aff550a0407d5c8aa4a
Cibi Jacob <cibi.jacob@citrusinformatics.com>
2025-10-31 10:25:33 +0000

    fix: update replay_status in sat_interaction_fhir_request table after the successful replay #2196 - updated the field names in sat_interaction_fhir_request table - updated sat_interaction_fhir_request_upserted function - updated pgTap function to test the updated fields and changes in the function

--- udi-prime/src/main/postgres/ingestion-center/migrations/migrate-basic-infrastructure.ts
+++ udi-prime/src/main/postgres/ingestion-center/migrations/migrate-basic-infrastructure.ts
@@ -131,8 +131,8 @@ const interactionFhirRequestSat = interactionHub.satelliteTable(
     group_hub_interaction_id: textNullable(),
     is_bundle_valid: boolean().default(false),
     bundle_type: textNullable(),
-    nyec_submitted_status: boolean().default(false),
-    submitted_on: dateTimeNullable(),
+    replay_status: textNullable(), //True/False/Null
+    replay_on: dateTimeNullable(),
     ...dvts.housekeeping.columns,
   },
 );
@@ -821,22 +821,22 @@ const migrateSP = pgSQLa.storedProcedure(
       ${interactionHttpRequestSat}
 
       ${interactionFhirRequestSat}
-      -- Check and add 'nyec_submitted_status' column if it does not exist
+      -- Check and add 'replay_status' column if it does not exist
       IF NOT EXISTS (SELECT 1 FROM information_schema.columns
                     WHERE table_schema = 'techbd_udi_ingress'
                     AND table_name='sat_interaction_fhir_request' 
-                    AND column_name='nyec_submitted_status') THEN
+                    AND column_name='replay_status') THEN
           ALTER TABLE techbd_udi_ingress.sat_interaction_fhir_request
-          ADD COLUMN nyec_submitted_status boolean DEFAULT false;
+          ADD COLUMN replay_status TEXT DEFAULT NULL;
       END IF;
 
-      -- Check and add 'submitted_on' column if it does not exist
+      -- Check and add 'replay_on' column if it does not exist
       IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
                     WHERE table_schema = 'techbd_udi_ingress'
                     AND table_name='sat_interaction_fhir_request' 
-                    AND column_name='submitted_on') THEN
+                    AND column_name='replay_on') THEN
           ALTER TABLE techbd_udi_ingress.sat_interaction_fhir_request
-          ADD COLUMN submitted_on timestamptz DEFAULT null;
+          ADD COLUMN replay_on timestamptz DEFAULT null;
       END IF;
 
       ${diagnosticDataledgerSat}
