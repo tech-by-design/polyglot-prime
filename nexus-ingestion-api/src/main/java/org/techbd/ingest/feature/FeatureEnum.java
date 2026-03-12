f0c69e3e4d519db12ea7cde9c5941f84ffd00b22
Shreeja Jobin <shreeja.jobin@citrusinformatics.com>
2026-03-12 14:31:17 +0530

    feat: update feature toggle defaults and add reset endpoint #2780
    - set SEND_HL7_ACK_ON_IDLE_TIMEOUT default to false
    - add endpoint to reset all feature flags to their default values

--- nexus-ingestion-api/src/main/java/org/techbd/ingest/feature/FeatureEnum.java
+++ nexus-ingestion-api/src/main/java/org/techbd/ingest/feature/FeatureEnum.java
@@ -1,21 +1,30 @@
 package org.techbd.ingest.feature;
 
 import org.togglz.core.Feature;
+import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.Label;
 import org.togglz.core.context.FeatureContext;
 
 public enum FeatureEnum implements Feature {
 
+    @EnabledByDefault
     @Label("Enable Debug Logging for Request Headers")
     DEBUG_LOG_REQUEST_HEADERS,
+
+    @EnabledByDefault
     @Label("Ignore SOAP MustUnderstand Headers")
     IGNORE_MUST_UNDERSTAND_HEADERS,
+
     @Label("Log Incoming Message")
     LOG_INCOMING_MESSAGE,
+
+    @EnabledByDefault
     @Label("Include NTE Segment in HL7 ACK Response")
     ADD_NTE_SEGMENT_TO_HL7_ACK,
+
     @Label("Send HL7 ACK/NACK When Idle Timeout Occurs")
     SEND_HL7_ACK_ON_IDLE_TIMEOUT;
+
     public static boolean isEnabled(Feature feature) {
         return FeatureContext.getFeatureManager().isActive(feature);
     }
