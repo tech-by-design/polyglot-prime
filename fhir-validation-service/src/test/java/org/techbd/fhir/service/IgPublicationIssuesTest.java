25b515a8bb8031b6c06682ab9021301a804e32e5
Shreeja Jobin <shreeja.jobin@citrusinformatics.com>
2026-08-12 10:37:46 +0530

    feat: update test-shinny IG to v1.9.4 #3413

--- fhir-validation-service/src/test/java/org/techbd/fhir/service/IgPublicationIssuesTest.java
+++ fhir-validation-service/src/test/java/org/techbd/fhir/service/IgPublicationIssuesTest.java
@@ -203,6 +203,17 @@ public class IgPublicationIssuesTest extends BaseIgValidationTest {
         void testValidateTestShinnyIG_NYScreeningResponseOPWDDExampleFile() throws IOException {
                 validateFile("test-shinny-examples/Bundle-NYScreeningResponseExampleOPWDD.json");
         }
+        @Test
+        @DisplayName("Validate Test SHIN-NY IG NY ScreeningResponse OPWDD ExampleFile")
+        void testValidateTestShinnyIG_PRAPAREALLAHCScreeningExampleFile() throws IOException {
+                validateFile("test-shinny-examples/Bundle-PRAPAREALLAHCScreeningExample.json");
+        }
+
+        @Test
+        @DisplayName("Validate Test SHIN-NY IG NY ScreeningResponse OPWDD ExampleFile")
+        void testValidateTestShinnyIG_PRAPAREANDAHCScreeningExampleFile() throws IOException {
+                validateFile("test-shinny-examples/Bundle-PRAPAREANDAHCScreeningExample.json");
+        }
                 
 
         private void validateFile(String filePath) throws IOException {
